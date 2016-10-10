066ebdf23eac1e64c746e839fee21f6f90a216ac
Brian Chipman <namedtuple@gmail.com>
2016-10-09 19:35:33 -0700

    Can now shoot left instead of only right, adjusted player control forces.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,16 +6,18 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 
-    public float speed = 3f;
-    public float maxSpeedX = 5f;
-    public float moveForce = 10f;
-    public float jumpForce = 400f;
+    private float maxSpeedX = 8f;
+    private float moveForce = 15f;
+    private float bulletForce = 20f;
+    private float jumpForce = 300f;
     public int score = 0;
     public GameObject scoreText;
     public GameObject bullet;
+    public Vector2 velocity;  // temporary, for debugging
 
     private bool jump = false;
     private bool grounded = false;
+    private bool facingRight = true;
     private float shotDelay = 0.10f;
     private SpriteRenderer spriteRenderer;
     private Animator animator;
@@ -33,7 +35,7 @@ public class Player : MonoBehaviour {
 
 
     private void Update() {
-        // Can jump off of enemies, platforms, or objects
+        // Can jump off of platforms, enemies, or objects
         bool grounded1 = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("PlatformLayer"));
         bool grounded2 = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("ObjectLayer"));
         bool grounded3 = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("EnemyLayer"));
@@ -48,9 +50,51 @@ public class Player : MonoBehaviour {
         if (transform.position.y <= -10) {
             transform.position = spawnPoint;
             GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-            spriteRenderer.flipX = false;
+            facingRight = true;
         }
+
+        velocity = GetComponent<Rigidbody2D>().velocity;
     }
+
+
+    private void FixedUpdate() {
+
+        // Get horizontal input
+        int h = (int) Input.GetAxisRaw("Horizontal");
+
+        // Set Speed animator parameter
+        animator.SetInteger("Speed", Mathf.Abs(h));
+
+        // If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeedX yet...
+        if (h * GetComponent<Rigidbody2D>().velocity.x < maxSpeedX) {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.right * h * moveForce);
+        }
+
+        if (h == 0) {
+            GetComponent<Rigidbody2D>().velocity *= 0.975f;
+        }
+
+        // If the player's horizontal velocity is greater than the maxSpeedX...
+        if (Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) > maxSpeedX) {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Sign(GetComponent<Rigidbody2D>().velocity.x) * maxSpeedX, GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+
+        // Handle flipping sprite if change directions
+        if (h > 0) { facingRight = true; }
+        else if (h < 0) { facingRight = false; }
+        spriteRenderer.flipX = !facingRight;
+
+
+        // Handle jumping
+        if (jump) {
+            animator.SetTrigger("Jump");
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
+            jump = false;
+        }
+
+    }
+
 
     private IEnumerator ShootTimer() {
         while (true) {
@@ -64,40 +108,29 @@ public class Player : MonoBehaviour {
         }
     }
 
+
     private void Fire() {
         // Create bullet instance near player's current position.
+        Vector3 bulletOffset;
+        Vector2 bulletVelocity;
+        Quaternion bulletQuaternion;
 
-
-        Vector3 newBulletPosition = transform.position + new Vector3(0.5f, 0, 0);
-        GameObject bulletInstance = Instantiate(bullet, newBulletPosition, Quaternion.AngleAxis(270, Vector3.forward)) as GameObject;
-        bulletInstance.gameObject.SetActive(true);
-        bulletInstance.GetComponent<Rigidbody2D>().velocity = new Vector2(10f, 0f);
-    }
-
-    private void FixedUpdate() {
-
-        // Get horizontal input
-        int h = (int) Input.GetAxisRaw("Horizontal");
-
-        // Set Speed animator parameter
-        animator.SetInteger("Speed", Mathf.Abs(h));
-
-        // If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeedX yet...
-        if (h * GetComponent<Rigidbody2D>().velocity.x < maxSpeedX) GetComponent<Rigidbody2D>().AddForce(Vector2.right * h * moveForce);
-
-        // If the player's horizontal velocity is greater than the maxSpeedX...
-        if (Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) > maxSpeedX) GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Sign(GetComponent<Rigidbody2D>().velocity.x) * maxSpeedX, GetComponent<Rigidbody2D>().velocity.y);
-
-        // Handle flipping sprite if change directions
-        if (h > 0) { spriteRenderer.flipX = false; }
-        else if (h < 0) { spriteRenderer.flipX = true; }
-
-        if (jump) {
-            animator.SetTrigger("Jump");
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
-            jump = false;
+        if (facingRight) {
+            bulletOffset = new Vector3(0.5f, 0, 0);
+            bulletVelocity = new Vector2(bulletForce, 0);
+            bulletQuaternion = Quaternion.AngleAxis(270, Vector3.forward);
+        }
+        else {
+            bulletOffset = new Vector3(-0.5f, 0, 0);
+            bulletVelocity = new Vector2(bulletForce * -1, 0);
+            bulletQuaternion = Quaternion.AngleAxis(90, Vector3.forward);
         }
 
+        Vector3 newBulletPosition = transform.position + bulletOffset;
+        GameObject bulletInstance = Instantiate(bullet, newBulletPosition, bulletQuaternion) as GameObject;
+        bulletInstance.gameObject.SetActive(true);
+        bulletInstance.GetComponent<Rigidbody2D>().velocity = bulletVelocity;
+        bulletInstance.transform.SetParent(this.transform);
     }
 
 }

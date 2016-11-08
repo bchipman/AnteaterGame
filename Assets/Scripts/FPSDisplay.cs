1f0b3530856ddb81cf4ea12d8f2de9792d72f94f
Brian Chipman <namedtuple@gmail.com>
2016-11-07 22:37:01 -0800

    Added some Google fonts, moved FPS text and projectile type dropdown UI elements.

--- Assets/Scripts/FPSDisplay.cs
+++ Assets/Scripts/FPSDisplay.cs
@@ -1,27 +1,31 @@
 // Source: http://wiki.unity3d.com/index.php/FramesPerSecond
 
 using UnityEngine;
-using System.Collections;
+using UnityEngine.UI;
 
 public class FPSDisplay : MonoBehaviour {
-    float deltaTime = 0.0f;
 
-    void Update() {
+    private float deltaTime = 0.0f;
+    private Text fpsText;
+
+    private void Start () {
+        fpsText = GetComponent<Text>();
+    }
+
+    private void Update() {
         deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
     }
 
-    void OnGUI() {
+    private void OnGUI() {
         int w = Screen.width, h = Screen.height;
+        fpsText.fontSize = h * 5 / 100;  // 5% of screen height
 
-        GUIStyle style = new GUIStyle();
-
-        Rect rect = new Rect(0, 0, w, h * 2 / 100);
-        style.alignment = TextAnchor.UpperLeft;
-        style.fontSize = h * 5 / 100;
-        style.normal.textColor = new Color(0.0f, 0.0f, 0.5f, 1.0f);
         float msec = deltaTime * 1000.0f;
         float fps = 1.0f / deltaTime;
-        string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
-        GUI.Label(rect, text, style);
+
+//        string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+        fpsText.text = string.Format("FPS: {0:0.}", fps);
     }
+
+
 }

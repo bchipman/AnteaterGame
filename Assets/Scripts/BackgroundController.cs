777fa38f433580ab3c842356226a7cbb4f3bebbd
Brian Chipman <namedtuple@gmail.com>
2016-11-09 22:51:03 -0800

    Added Allisah's background and anteater sprite; player now controls the anteater instead of the unity robot.  Resized the placeholder enemies, created new animation controller for anteater.

--- Assets/Scripts/BackgroundController.cs
+++ Assets/Scripts/BackgroundController.cs
@@ -7,6 +7,7 @@ public class BackgroundController : MonoBehaviour {
     private SpriteRenderer spriteRendererSky0;
     private SpriteRenderer spriteRendererSky1;
     private SpriteRenderer spriteRendererSky2;
+    private const float yVal = 10.1f;
 
 	void Start () {
         spriteRendererSky0 = transform.Find("/Environment/Sky/Sky0").gameObject.GetComponent<SpriteRenderer>();
@@ -17,12 +18,12 @@ public class BackgroundController : MonoBehaviour {
         Bounds boundsSky2 = spriteRendererSky2.bounds;
 
         // ahead
-        spriteRendererSky2.transform.position = new Vector3(boundsSky1.max.x + boundsSky2.extents.x - 0.1f, 0, 0);
+        spriteRendererSky2.transform.position = new Vector3(boundsSky1.max.x + boundsSky2.extents.x, yVal, 0);
 
         // behind
         boundsSky0 = spriteRendererSky0.bounds;
         boundsSky1 = spriteRendererSky1.bounds;
-        spriteRendererSky0.transform.position = new Vector3(boundsSky1.min.x - boundsSky0.extents.x + 0.1f, 0, 0);
+        spriteRendererSky0.transform.position = new Vector3(boundsSky1.min.x - boundsSky0.extents.x, yVal, 0);
 	}
 
 	void Update () {
@@ -32,7 +33,7 @@ public class BackgroundController : MonoBehaviour {
 
         if (player.transform.position.x > b1.max.x) {
             // 0 gets moved to end of 2, then all renumbered
-            spriteRendererSky0.transform.position = new Vector3(b2.max.x + b0.extents.x - 0.1f, 0, 0);
+            spriteRendererSky0.transform.position = new Vector3(b2.max.x + b0.extents.x, yVal, 0);
             SpriteRenderer tempOld1 = spriteRendererSky1;
             SpriteRenderer tempOld2 = spriteRendererSky2;
             spriteRendererSky2 = spriteRendererSky0;
@@ -42,7 +43,7 @@ public class BackgroundController : MonoBehaviour {
 
         else if (player.transform.position.x < b1.min.x) {
             // 2 gets moved to front of 0, then all renumbered
-            spriteRendererSky2.transform.position = new Vector3(b0.min.x - b2.extents.x + 0.1f, 0, 0);
+            spriteRendererSky2.transform.position = new Vector3(b0.min.x - b2.extents.x, yVal, 0);
             SpriteRenderer tempOld0 = spriteRendererSky0;
             SpriteRenderer tempOld1 = spriteRendererSky1;
             spriteRendererSky0 = spriteRendererSky2;

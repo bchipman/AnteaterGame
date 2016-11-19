62aa455c090c2c4424e7196bbb6a595d5bd1b19d
Eleanor <emeriwet@uci.edu>
2016-11-18 18:24:27 -0800

    added a mute option to options menu

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+
+	float masterVolumePercent = 1;
+	float musicVolumePercent = 1;
+	float sfxVolumePercent = 1;
+
+	public static AudioManager instance;
+
+	AudioSource playerAS;
+	//AudioSource[] musicSources;
+	//int activeMusicSourceIndex;
+
+	void Awake(){
+		
+		instance = this;
+
+		playerAS = new AudioSource();
+		GameObject newMusicSource = new GameObject ("Music Source 1");
+		newMusicSource.transform.parent = transform;
+		//playerAS = GetComponent<AudioSource> ();
+		/*
+		musicSources = new AudioSource[2];
+		for (int i = 0; i < 2; i++) {
+			GameObject newMusicSource = new GameObject ("Music source" + (i + 1));
+			musicSources[i] = newMusicSource.AddComponent<AudioSource> ();
+			newMusicSource.transform.parent = transform;
+		}
+		*/
+	}
+
+	public void PlayMusic(AudioClip clip, float fadeDuration = 1) {
+		//activeMusicSourceIndex = 1 - activeMusicSourceIndex;
+		//musicSources [activeMusicSourceIndex].clip = clip;
+		//musicSources [activeMusicSourceIndex].Play ();
+
+		//StartCoroutine [AnimateMusicCrossfade (fadeDuration)];
+
+		playerAS.Play();
+	}
+
+	public void PlaySound(AudioClip clip, Vector2 pos){
+		AudioSource.PlayClipAtPoint (clip, pos, sfxVolumePercent * masterVolumePercent);
+	}
+
+
+	/*
+	 * IEnumerator AnimateMusicCrossfade(float duration) {
+		float percent = 0;
+
+		while (percent < 1) {
+			percent += Time.deltaTime * 1 / duration;
+			musicSources [activeMusicSourceIndex].volume = Mathf.Lerp (0, musicVolumePercent * masterVolumePercent, percent);
+			musicSources [1-activeMusicSourceIndex].volume = Mathf.Lerp (0, musicVolumePercent * masterVolumePercent, 0,  percent);
+			yield return null;
+		}
+	}
+	*/
+}
--- Assets/Scripts/TitleScreenManager.cs
+++ Assets/Scripts/TitleScreenManager.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 public class TitleScreenManager: MonoBehaviour {
 
     private Button playButton;
+	private bool isMuted = false;
+	private Toggle muted;
 
 	public GameObject mainMenuHolder;
 	public GameObject optionsMenuHolder;
@@ -12,6 +14,8 @@ public class TitleScreenManager: MonoBehaviour {
 	private void Start () {
         playButton = GameObject.Find("PlayButton").GetComponent<Button>();
         playButton.onClick.AddListener(LoadLevel1);
+
+		//AudioListener.volume = 0; //works to turn off sound automatically
     }
 	
     private void LoadLevel1() {
@@ -21,6 +25,10 @@ public class TitleScreenManager: MonoBehaviour {
 	public void OptionsMenu() {
 		mainMenuHolder.SetActive (false);
 		optionsMenuHolder.SetActive (true);
+
+		muted = GameObject.Find ("/Canvas/OptionsMenu/MuteToggle").GetComponent<Toggle> ();
+		muted.enabled = true;
+		muted.onValueChanged.AddListener (ToggleSound);
 	}
 
 	public void MainMenu() {
@@ -28,9 +36,18 @@ public class TitleScreenManager: MonoBehaviour {
 		optionsMenuHolder.SetActive (false);
 	}
 
+	public void ToggleSound(bool value) {
+		//isMuted = GameObject.Find ("MuteToggle").GetComponent<Toggle> ();
+		if (value)
+			AudioListener.volume = 0;
+		else
+			AudioListener.volume = 1;
+	}
+
+	/*
 	public void SetMasterVolume(float value) {
 
-	}
+	}*/
 
 	public void SetMusicVolume(float value) {
 

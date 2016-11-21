712bc5a1a7b3f3aefe3572d1205437479b12adb3
Brian Chipman <namedtuple@gmail.com>
2016-11-21 14:55:33 -0800

    Changed sprite image of block prefab.  Moved location of generated coins.

--- Assets/Scripts/CoinGenerator.cs
+++ Assets/Scripts/CoinGenerator.cs
@@ -5,16 +5,17 @@ using UnityEngine.UI;
 public class CoinGenerator : MonoBehaviour {
 
     public GameObject coin;
-    private Vector2 initialPos = new Vector2(-8f, -1.5f);
+//    private Vector2 initialPos = new Vector2(-8f, -1.5f);
+    private Vector2 initialPos = new Vector2(11f, 2f);
     private int xSep = 1;
     private int ySep = 1;
 
     private void Start () {
         // Create some coins
         for (int col = 0; col < 10; col++) {
-	        for (int row = 0; row < 5; row++) {
+	        for (int row = 0; row < 3; row++) {
 	            GameObject instance = Instantiate(coin, new Vector3(col * xSep + initialPos.x, row * ySep +initialPos.y, 0f), Quaternion.identity) as GameObject;
-                instance.transform.SetParent(transform.Find("/Collectables"));
+                instance.transform.SetParent(transform.Find("/Collectables/Coins"));
             }
         }
 	}

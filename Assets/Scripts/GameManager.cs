9577bbc26242f34fcd645a22434fc8aa9761ee03
Brian Chipman <namedtuple@gmail.com>
2016-11-26 22:45:21 -0800

    Repositioned UI elements in level1.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -25,9 +25,9 @@ public class GameManager : MonoBehaviour {
         scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
         timeText = GameObject.Find("TimeText").GetComponent<Text>();
         deathText = GameObject.Find("DeathText").GetComponent<Text>();
-        fpsText = GameObject.Find("FPSText").GetComponent<Text>();
+//        fpsText = GameObject.Find("FPSText").GetComponent<Text>();
         closeButton = GameObject.Find("CloseButton").GetComponent<Button>();
-        projectileTypeDropdown = GameObject.Find("ProjectileTypeDropdown").GetComponent<Dropdown>();
+//        projectileTypeDropdown = GameObject.Find("ProjectileTypeDropdown").GetComponent<Dropdown>();
         bookCirclesRectTransLi = GameObject.Find("/Canvas/Circles").GetComponentsInChildren<RectTransform>();
         bookCirclesRectTransLi = bookCirclesRectTransLi.ToList().Where(r => new Regex(@"Circle\d").IsMatch(r.name)).ToArray();
         closeButton.onClick.AddListener(LoadTitleScreen);
@@ -40,34 +40,34 @@ public class GameManager : MonoBehaviour {
     }
 
     private void OnGUI() {
-        int w = Screen.width, h = Screen.height;
-        float smallSize = 0.07f;
-        float largeSize = 0.2f;
-
-        fpsText.fontSize = Mathf.RoundToInt(h * smallSize);
-        scoreText.fontSize = Mathf.RoundToInt(h * smallSize);
-        timeText.fontSize = Mathf.RoundToInt(h * smallSize);
-        deathText.fontSize = Mathf.RoundToInt(h * largeSize);
-        
-        closeButton.GetComponent<RectTransform>().sizeDelta = new Vector2(h * 0.12f, h * 0.12f);
-        closeButton.GetComponent<RectTransform>().anchorMin = new Vector2(1f, 1f);
-        closeButton.GetComponent<RectTransform>().anchorMax = new Vector2(1f, 1f);
-        Vector2 size = closeButton.GetComponent<RectTransform>().sizeDelta;
-        Vector2 halfSize = new Vector2(-1*size.x/2, -1*size.y/2);
-        closeButton.GetComponent<RectTransform>().anchoredPosition = halfSize;
-
-        Text closeButtonText = GameObject.Find("CloseButton/Text").GetComponent<Text>();
-        closeButtonText.fontSize = Mathf.RoundToInt(h * 0.1f);
-
-        float fps = 1.0f / deltaTime;
-        fpsText.text = string.Format("FPS: {0:0.}", fps);
-
-        for (int i = 0; i < bookCirclesRectTransLi.Length; i++) {
-            var rectTrans = bookCirclesRectTransLi[i];
-            rectTrans.sizeDelta = new Vector2(h * 0.1f, h * 0.1f); // radius = 10% of screen height
-            rectTrans.anchoredPosition = new Vector2(w * 0.7f + (i * rectTrans.sizeDelta.x) + (i * 0.002f * h), h * -0.05f - 0.005f * h); // 70% to right, 0.5% from top
-            rectTrans.transform.FindChild("Book").GetComponent<RectTransform>().sizeDelta = new Vector2(w * 0.025f, w * 0.025f * 1.7f);
-        }
+//        int w = Screen.width, h = Screen.height;
+//        float smallSize = 0.07f;
+//        float largeSize = 0.2f;
+//
+//        fpsText.fontSize = Mathf.RoundToInt(h * smallSize);
+//        scoreText.fontSize = Mathf.RoundToInt(h * smallSize);
+//        timeText.fontSize = Mathf.RoundToInt(h * smallSize);
+//        deathText.fontSize = Mathf.RoundToInt(h * largeSize);
+//        
+//        closeButton.GetComponent<RectTransform>().sizeDelta = new Vector2(h * 0.12f, h * 0.12f);
+//        closeButton.GetComponent<RectTransform>().anchorMin = new Vector2(1f, 1f);
+//        closeButton.GetComponent<RectTransform>().anchorMax = new Vector2(1f, 1f);
+//        Vector2 size = closeButton.GetComponent<RectTransform>().sizeDelta;
+//        Vector2 halfSize = new Vector2(-1*size.x/2, -1*size.y/2);
+//        closeButton.GetComponent<RectTransform>().anchoredPosition = halfSize;
+//
+//        Text closeButtonText = GameObject.Find("CloseButton/Text").GetComponent<Text>();
+//        closeButtonText.fontSize = Mathf.RoundToInt(h * 0.1f);
+//
+//        float fps = 1.0f / deltaTime;
+//        fpsText.text = string.Format("FPS: {0:0.}", fps);
+//
+//        for (int i = 0; i < bookCirclesRectTransLi.Length; i++) {
+//            var rectTrans = bookCirclesRectTransLi[i];
+//            rectTrans.sizeDelta = new Vector2(h * 0.1f, h * 0.1f); // radius = 10% of screen height
+//            rectTrans.anchoredPosition = new Vector2(w * 0.7f + (i * rectTrans.sizeDelta.x) + (i * 0.002f * h), h * -0.05f - 0.005f * h); // 70% to right, 0.5% from top
+//            rectTrans.transform.FindChild("Book").GetComponent<RectTransform>().sizeDelta = new Vector2(w * 0.025f, w * 0.025f * 1.7f);
+//        }
 
     }
 
@@ -113,11 +113,12 @@ public class GameManager : MonoBehaviour {
     }
 
     public GameObject GetCurrentProjectileType() {
-        switch (projectileTypeDropdown.value) {
-            case 0: return bulletPrefab;
-            case 1: return zotBubblePrefab;
-            default: return dirtPrefab;
-        }
+//        switch (projectileTypeDropdown.value) {
+//            case 0: return bulletPrefab;
+//            case 1: return zotBubblePrefab;
+//            default: return dirtPrefab;
+//        }
+        return dirtPrefab;
     }
 
     public void SpawnBookCollectable(Vector3 position) {

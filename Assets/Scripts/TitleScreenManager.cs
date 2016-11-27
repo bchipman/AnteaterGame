276cd0e4df8be1b5f72bdebfced2c3e3312aa815
Brian Chipman <namedtuple@gmail.com>
2016-11-26 21:24:35 -0800

    Added Allisah's title screen and options screen backgrounds to title screen scene.

--- Assets/Scripts/TitleScreenManager.cs
+++ Assets/Scripts/TitleScreenManager.cs
@@ -4,61 +4,76 @@ using UnityEngine.UI;
 
 public class TitleScreenManager: MonoBehaviour {
 
-    private Toggle mutedToggle;
-    private Text titleText;
-    private RectTransform anteaterImageRectTransform;
     private TitleScreenState titleScreenState;
-
     public GameObject mainMenuHolder;
 	public GameObject optionsMenuHolder;
 
+
 	private void Start () {
-        titleText = GameObject.Find("TitleText").GetComponent<Text>();
-	    anteaterImageRectTransform = GameObject.Find("AnteaterImage").GetComponent<RectTransform>();
 	    titleScreenState = GameObject.Find("TitleScreenState").GetComponent<TitleScreenState>();
 	}
 
-    private void OnGUI() {
-        int w = Screen.width, h = Screen.height;
-        float smallSize = 0.12f;
-        float largeSize = 0.15f;
-
-        titleText.fontSize = Mathf.RoundToInt(h * largeSize);
-        titleText.GetComponent<RectTransform>().sizeDelta = new Vector2(w, h * 0.2f);
-        Vector2 titleTextSize = titleText.GetComponent<RectTransform>().sizeDelta;
-        Vector2 titleTextHalfSize = new Vector2(0, -1 * titleTextSize.y / 2);
-        titleText.GetComponent<RectTransform>().anchoredPosition = titleTextHalfSize;
-        titleText.GetComponent<RectTransform>().offsetMin = new Vector2(0, titleText.GetComponent<RectTransform>().offsetMin.y);
-        titleText.GetComponent<RectTransform>().offsetMax = new Vector2(0, titleText.GetComponent<RectTransform>().offsetMax.y);
-
-        anteaterImageRectTransform.localScale = new Vector2(1, 1);
-        anteaterImageRectTransform.sizeDelta = new Vector2(w, h * 0.9f);
-
-        GameObject playButtonObj = GameObject.Find("PlayButton");
-        if (playButtonObj != null) {
-            Button playButton = playButtonObj.GetComponent<Button>();
-            playButton.GetComponent<RectTransform>().sizeDelta = new Vector2(w * 0.65f, h * 0.15f);
-            playButton.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -1 * h * 0.6f);
-            playButtonObj.GetComponentInChildren<Text>().fontSize = Mathf.RoundToInt(h * smallSize);
-        }
-
-        GameObject optionsButtonObj = GameObject.Find("OptionsButton");
-        if (optionsButtonObj != null) {
-            Button optionsButton = optionsButtonObj.GetComponent<Button>();
-            optionsButton.GetComponent<RectTransform>().sizeDelta = new Vector2(w * 0.65f, h * 0.15f);
-            optionsButton.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -1 * h * 0.8f);
-            optionsButtonObj.GetComponentInChildren<Text>().fontSize = Mathf.RoundToInt(h * smallSize);
-        }
-
-        GameObject backButtonObj = GameObject.Find("BackButton");
-        if (backButtonObj != null) {
-            Button backButton = backButtonObj.GetComponent<Button>();
-            backButton.GetComponent<RectTransform>().sizeDelta = new Vector2(w * 0.65f, h * 0.15f);
-            backButton.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -1 * h * 0.8f);
-            backButtonObj.GetComponentInChildren<Text>().fontSize = Mathf.RoundToInt(h * smallSize);
-        }
-    }
-
+//    private void OnGUI() {
+//        int w = Screen.width;
+//        int h = Screen.height;
+//        float smallFontSize = 0.06f;
+//
+////        GameObject mainMenuImageObj = GameObject.Find("MainMenuImage");
+////        if (mainMenuImageObj != null) {
+////            RectTransform mainMenuImageRectTransform = mainMenuImageObj.GetComponent<RectTransform>();
+////            mainMenuImageRectTransform.sizeDelta = new Vector2(w, h);
+////            Vector2 mainMenuImageSize = mainMenuImageRectTransform.sizeDelta;
+////            Vector2 mainMenuImageHalfSize = new Vector2(-1 * mainMenuImageSize.x / 2, mainMenuImageSize.y / 2);
+////            mainMenuImageRectTransform.anchoredPosition = mainMenuImageHalfSize;
+////        }
+////
+////        GameObject optionsMenuImageObj = GameObject.Find("OptionsMenuImage");
+////        if (optionsMenuImageObj != null) {
+////            RectTransform optionsImageRectTransform = optionsMenuImageObj.GetComponent<RectTransform>();
+////            optionsImageRectTransform.sizeDelta = new Vector2(w, h);
+////            Vector2 optionsMenuImageSize = optionsImageRectTransform.sizeDelta;
+////            Vector2 optionsMenuImageHalfSize = new Vector2(-1 * optionsMenuImageSize.x / 2, optionsMenuImageSize.y / 2);
+////            optionsImageRectTransform.anchoredPosition = optionsMenuImageHalfSize;
+////        }
+//
+//        GameObject playButtonObj = GameObject.Find("PlayButton");
+//        if (playButtonObj != null) {
+//            Button playButton = playButtonObj.GetComponent<Button>();
+//            playButton.GetComponent<RectTransform>().sizeDelta = new Vector2(w * 0.40f, h * 0.10f);
+//            playButton.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -1 * h * 0.65f);
+//            playButtonObj.GetComponentInChildren<Text>().fontSize = Mathf.RoundToInt(h * smallFontSize);
+//        }
+//
+//        GameObject optionsButtonObj = GameObject.Find("OptionsButton");
+//        if (optionsButtonObj != null) {
+//            Button optionsButton = optionsButtonObj.GetComponent<Button>();
+//            optionsButton.GetComponent<RectTransform>().sizeDelta = new Vector2(w * 0.40f, h * 0.10f);
+//            optionsButton.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -1 * h * 0.8f);
+//            optionsButtonObj.GetComponentInChildren<Text>().fontSize = Mathf.RoundToInt(h * smallFontSize);
+//        }
+//
+//        GameObject backButtonObj = GameObject.Find("BackButton");
+//        if (backButtonObj != null) {
+//            Button backButton = backButtonObj.GetComponent<Button>();
+//            backButton.GetComponent<RectTransform>().sizeDelta = new Vector2(w * 0.40f, h * 0.10f);
+//            backButton.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -1 * h * 0.8f);
+//            backButtonObj.GetComponentInChildren<Text>().fontSize = Mathf.RoundToInt(h * smallFontSize);
+//        }
+//
+////        GameObject muteToggleObj = GameObject.Find("MuteToggle");
+////        if (muteToggleObj != null) {
+////            GameObject muteToggleCheckmarkObj = GameObject.Find("MuteToggle/Background/Checkmark");
+////            Debug.Log(muteToggleCheckmarkObj);
+////            RectTransform muteToggleCheckmarkRectTransform = muteToggleCheckmarkObj.GetComponent<RectTransform>();
+//////            muteToggleCheckmarkRectTransform.sizeDelta = new Vector2(w*0.40f, h * 0.10f);
+////            muteToggleCheckmarkRectTransform.sizeDelta = new Vector2(w, h);
+////
+//////            RectTransform muteToggleRectTransform = muteToggleObj.GetComponent<RectTransform>();
+//////            muteToggleRectTransform.GetComponent<RectTransform>().sizeDelta = new Vector2(w * 0.40f, h * 0.10f);
+//////            muteToggleRectTransform.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -1 * h * 0.8f);
+//////            backButtonObj.GetComponentInChildren<Text>().fontSize = Mathf.RoundToInt(h * smallFontSize);
+////        }
+//    }
 
     public void LoadLevel1() {
         SceneManager.LoadScene("Level1");
@@ -68,7 +83,7 @@ public class TitleScreenManager: MonoBehaviour {
 		mainMenuHolder.SetActive (false);
 		optionsMenuHolder.SetActive (true);
 
-		mutedToggle = GameObject.Find ("/Canvas/OptionsMenu/MuteToggle").GetComponent<Toggle> ();
+		Toggle mutedToggle = GameObject.Find ("/Canvas/OptionsMenu/MuteToggle").GetComponent<Toggle> ();
 		mutedToggle.enabled = true;
 	    mutedToggle.isOn = titleScreenState.isMuted;
 		mutedToggle.onValueChanged.AddListener (ToggleSound);
